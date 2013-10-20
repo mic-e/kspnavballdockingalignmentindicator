5cddb2228b425336b5d74320531ada338701e217
Michael Enßlin <michael@ensslin.cc>
2013-10-20 23:30:06 +0200

    initial commit
    working version (KSP 0.22)

---
+++ Makefile
@@ -0,0 +1,26 @@
+DLL=NavBallDockingAlignmentIndicator.dll
+SRC=NavBallDockingAlignmentIndicator.cs
+TGZ=NavBallDockingAlignmentIndicator.tar.gz
+
+$(DLL): $(SRC)
+	@test "$(KSP_DIR)" || echo 'You need to set $$KSP_DIR'
+	@test "$(KSP_DIR)"
+	dmcs -r:"$(KSP_DIR)/KSP_Data/Managed/Assembly-CSharp.dll" -r:"$(KSP_DIR)/KSP_Data/Managed/UnityEngine.dll" -t:library $(SRC) -out:$(DLL)
+
+install:
+	@test "$(KSP_DIR)" || echo 'You need to set $$KSP_DIR'
+	@test "$(KSP_DIR)"
+	cp $(DLL) "$(KSP_DIR)/KSP_Data"
+
+uninstall:
+	@test "$(KSP_DIR)" || echo 'You need to set $$KSP_DIR'
+	@test "$(KSP_DIR)"
+	rm "$(KSP_DIR)/KSP_Data/$(DLL)"
+
+clean:
+	rm -f $(DLL) $(TGZ)
+
+tgz: $(DLL)
+	tar cz license Makefile readme $(SRC) $(DLL) > $(TGZ)
+
+.PHONY: install uninstall clean tar
---
+++ NavBallDockingAlignmentIndicator.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+
+[KSPAddon(KSPAddon.Startup.Flight, false)]
+public class NavBallDockingAlignment : MonoBehaviour
+{
+    private NavBall navBallBehaviour;
+	private GameObject indicator;
+
+	private void dbg (string msg)
+	{
+		File.AppendAllText("/tmp/dbg", msg + "\n");
+	}
+
+    public void Start ()
+	{
+		//get navball object
+		GameObject navBall = GameObject.Find("NavBall");
+		Transform navBallVectorsPivotTransform = navBall.transform.FindChild("vectorsPivot");
+		navBallBehaviour = navBall.GetComponent<NavBall>();
+
+		//get indicator texture (use one of the maneuver thingies)
+		ManeuverGizmo maneuverGizmo = MapView.ManeuverNodePrefab.GetComponent<ManeuverGizmo>();
+		ManeuverGizmoHandle maneuverGizmoHandle = maneuverGizmo.handleNormal;
+		Transform transform = maneuverGizmoHandle.flag;
+		Renderer renderer = transform.renderer;
+		Material maneuverTexture = renderer.sharedMaterial;
+
+		//create alignment indicator game object
+		indicator = Create2DObject (
+			name: "navballalignmentindicator",
+			size: 0.025f,
+			col: new Color (1f, 0f, 0),
+			texture: maneuverTexture,
+			textureScale: Vector2.one / 3,
+			textureOffset: new Vector2 (0f / 3f, 2f / 3f),
+			parentTransform: navBallVectorsPivotTransform,
+			layer: 12 //the navball layer
+		);
+    }
+
+    private GameObject Create2DObject(
+		string name,
+		float size,
+		Color col,
+		Material texture,
+        Vector2 textureScale,
+		Vector2 textureOffset,
+        Transform parentTransform,
+		int layer)
+    {
+		GameObject o = new GameObject(name);
+        Mesh m = new Mesh();
+        MeshFilter meshFilter = o.AddComponent<MeshFilter>();
+        o.AddComponent<MeshRenderer>();
+
+        const float uvize = 1f;
+
+        Vector3 p0 = new Vector3(-size, 0, size);
+        Vector3 p1 = new Vector3(size, 0, size);
+        Vector3 p2 = new Vector3(-size, 0, -size);
+        Vector3 p3 = new Vector3(size, 0, -size);
+
+        m.vertices = new[]
+        {
+            p0, p1, p2,
+        	p1, p3, p2
+        };
+
+        m.triangles = new[]
+        {
+        	0, 1, 2,
+            3, 4, 5
+        };
+
+        Vector2 uv1 = new Vector2(0, 0);
+        Vector2 uv2 = new Vector2(uvize, uvize);
+        Vector2 uv3 = new Vector2(0, uvize);
+        Vector2 uv4 = new Vector2(uvize, 0);
+
+        m.uv = new[]{
+        	uv1, uv4, uv3,
+            uv4, uv2, uv3
+        };
+           
+        m.RecalculateNormals();
+        m.RecalculateBounds();
+        m.Optimize();
+            
+        meshFilter.mesh = m;
+
+		o.layer = layer;
+        o.transform.parent = parentTransform;
+        o.transform.localPosition = Vector3.zero;
+        o.transform.localRotation = Quaternion.Euler(90f, 180f, 0);
+
+        o.renderer.sharedMaterial = new Material(texture);
+        o.renderer.sharedMaterial.mainTextureScale = textureScale;
+        o.renderer.sharedMaterial.mainTextureOffset = textureOffset;
+        o.renderer.sharedMaterial.color = col;
+
+		return o;
+    }
+
+    public void LateUpdate()
+    {
+        if (FlightGlobals.ready == false)
+            return;
+
+		if(FlightGlobals.fetch != null) {
+			if(FlightGlobals.fetch.VesselTarget != null) {
+				if(FlightGlobals.fetch.VesselTarget is ModuleDockingNode) {
+					ModuleDockingNode targetPort = FlightGlobals.fetch.VesselTarget as ModuleDockingNode;
+					Transform targetTransform = targetPort.transform;
+					Transform selfTransform = FlightGlobals.ActiveVessel.ReferenceTransform;
+
+					//indicator position
+					Vector3 targetPortOutVector = targetTransform.up.normalized;
+					Vector3 targetPortInVector = -targetPortOutVector;
+					Vector3 rotatedTargetPortInVector = navBallBehaviour.attitudeGymbal * targetPortInVector;
+					indicator.transform.localPosition = rotatedTargetPortInVector * navBallBehaviour.progradeVector.localPosition.magnitude;
+
+					//indicator rotation
+					Vector3 v1 = Vector3.Cross(selfTransform.up, targetTransform.forward);
+					Vector3 v2 = Vector3.Cross(selfTransform.up, selfTransform.forward);
+					float ang = Vector3.Angle(v1, v2);
+					if (Vector3.Dot(selfTransform.up, Vector3.Cross(v1, v2)) < 0) {
+						ang = -ang;
+					}
+					indicator.transform.rotation = Quaternion.Euler(90 + ang, 90, 270);
+
+					//indicator visibility (invisible if on back half sphere)
+					indicator.SetActive(indicator.transform.localPosition.z > 0.0d);
+
+					return;
+				} 
+			}
+		}
+
+		//no docking port is currently selected
+		indicator.SetActive(false);
+		return;
+    }
+}
